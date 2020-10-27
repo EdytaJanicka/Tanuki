06f4b584b5dedea5fbb113326f88b7b95195d564
Edyta <edyta.janicka0@gmail.com>
2020-10-27 20:38:24 +0100

    UI - not working yet

--- Tanuki Tracz/Assets/Scripts/CameraMovement.cs
+++ Tanuki Tracz/Assets/Scripts/CameraMovement.cs
@@ -10,7 +10,7 @@ public class CameraMovement : MonoBehaviour
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.Locked;
     }
     
     void Update()
