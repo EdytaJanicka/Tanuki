f9f50e750daaf538456a1fed35daffac89cd1d24
Edyta <edyta.janicka0@gmail.com>
2020-11-07 17:45:48 +0100

    Dialogi plus MiniGra1

--- Tanuki Tracz/Assets/Scripts/MotherConversation.cs
+++ Tanuki Tracz/Assets/Scripts/MotherConversation.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DialogueEditor;
+using UnityEngine.UI;
 
 public class MotherConversation : MonoBehaviour
 {
     public GameObject mother;
     public GameObject player;
     public NPCConversation myConversation;
+    public NPCConversation myConversation1;
+    public NPCConversation myConversation2;
     public GameObject buttonE;
     [SerializeField] private bool isTalking = false;
     private bool inAField = false;
     public GameObject movement;
-    public GameObject camera;
+    public GameObject camera1;
+    public int isAgreedInConversation = 0;
+    public GameObject quest1;
 
 
     void Start()
@@ -23,15 +28,21 @@ public class MotherConversation : MonoBehaviour
    
     void Update()
     {
-    
-        if(Input.GetKeyDown(KeyCode.E) && inAField == true && isTalking == false)
+
+        if (Input.GetKeyDown(KeyCode.E) && inAField == true && isTalking == false && isAgreedInConversation == 0)
         {
             ConversationManager.Instance.StartConversation(myConversation);
             buttonE.SetActive(false);
             isTalking = true;
             StopMoving();
         }
-        
+        if (Input.GetKeyDown(KeyCode.E) && inAField == true && isTalking == false && isAgreedInConversation == 1)
+        {
+            ConversationManager.Instance.StartConversation(myConversation1);
+            buttonE.SetActive(false);
+            isTalking = true;
+            StopMoving();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -54,13 +65,18 @@ public class MotherConversation : MonoBehaviour
 
     public void StopMoving()
     {
-        camera.GetComponent<CameraMovement>().enabled = false;
+        camera1.GetComponent<CameraMovement>().enabled = false;
         movement.GetComponent<PlayerMovement>().stopMovement = false;
     }
 
     public void StartMoving()
     {
-        camera.GetComponent<CameraMovement>().enabled = true;
+        camera1.GetComponent<CameraMovement>().enabled = true;
         movement.GetComponent<PlayerMovement>().stopMovement = true;
     }
+    public void Agreed()
+    {
+        isAgreedInConversation += 1;
+    }
+
 }
