58a52f4eb7cefcf0fff415959b0fe872cddd19af
Edyta <edyta.janicka0@gmail.com>
2020-11-17 02:06:46 +0100

    New

---
+++ Tanuki Tracz/Assets/Scripts/JumpToMiniGame2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpToMiniGame2 : MonoBehaviour
+{
+    public GameObject buttonE;
+    private bool inAField = false;
+    public MotherConversation mother;
+    public GameObject mainCamera;
+    public GameObject cameraCarpet;
+
+    void Start()
+    {
+         buttonE.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && inAField == true && mother.isAgreedInConversation == 3)
+        {
+           
+            buttonE.SetActive(false);
+
+        }
+    }
+}
